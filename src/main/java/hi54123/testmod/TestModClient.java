253eaa4469eca3cf400a85d3d6b2de2394419b03
BuildTools <unconfigured@null.spigotmc.org>
2023-07-01 19:18:48 +0800

    Change Fabric Data

---
+++ src/main/java/hi54123/testmod/TestModClient.java
@@ -0,0 +1,11 @@
+package hi54123.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+	
+	@Override
+	public void onInitializeClient() {
+	
+	}
+}
