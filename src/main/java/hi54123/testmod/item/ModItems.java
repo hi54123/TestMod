383bbeed38f0f8232330c6c18ce8a3f71ee8e16a
BuildTools <unconfigured@null.spigotmc.org>
2023-07-02 10:56:20 +0800

    Update item

--- src/main/java/hi54123/testmod/item/ModItems.java
+++ src/main/java/hi54123/testmod/item/ModItems.java
@@ -10,6 +10,8 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 	public static final Item MODITEM1 = rigisterItem("moditem1",
 			new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+	public static final Item MODITEM2 = rigisterItem("moditem2",
+			new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 	
 	private static Item rigisterItem(String name, Item item) {
 		return Registry.register(Registry.ITEM, new Identifier(TestMod.MOD_ID, name), item);
